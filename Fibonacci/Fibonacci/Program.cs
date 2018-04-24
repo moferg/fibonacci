412e57fbb13e968a1104d7d9923e053e11a28c28
Marshall Ferguson <moferg01@louisville.edu>
2018-04-24 09:05:42 -0400

    Added pseudocode outline and outlined Fibonacci()

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -10,6 +10,21 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            // Desired output: 1 1 2 3 5 8 13 21 34 55
+            // for loop i < 10
+                // Fibonacci(i);
+        }
+
+        // Algorithm: n + (n - 1)
+        static int Fibonacci(int n)
+        {
+            //base case
+            if(n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n) + Fibonacci(n - 1);
         }
     }
 }
