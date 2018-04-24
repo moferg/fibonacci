6027fecc0fdef0a6c4e7db1cce6712f2d4e17ba8
Marshall Ferguson <moferg01@louisville.edu>
2018-04-24 09:20:41 -0400

    Program prints out desired output

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -11,8 +11,12 @@ namespace Fibonacci
         static void Main(string[] args)
         {
             // Desired output: 1 1 2 3 5 8 13 21 34 55
-            // for loop i < 10
-                // Fibonacci(i);
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+
+            Console.ReadKey();
         }
 
         // Algorithm: n + (n - 1)
@@ -24,7 +28,7 @@ namespace Fibonacci
                 return 1;
             }
 
-            return Fibonacci(n) + Fibonacci(n - 1);
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
     }
 }
